66e858342fc69dc5643315df910fed23f781f144
keithslater <keith@anvilstl.com>
2015-12-13 15:10:50 -0600

    added check for whois

--- whois.py
+++ whois.py
@@ -8,6 +8,11 @@ class Whois(BotPlugin):
    def activate(self):
         super(Whois, self).activate()
 
+	try:
+		Popen(['whois'], stdout=PIPE, stderr=STDOUT).communicate()[0]
+	except Exception as e:
+		self.warn_admins("This plugin uses whois and it wasn't found on your system")
+
    @botcmd
    def whois(self, mess, args):
       return Popen(['whois'] + args.split(), stdout=PIPE, stderr=STDOUT).communicate()[0].decode('utf-8')
