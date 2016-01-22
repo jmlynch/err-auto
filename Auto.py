8e6359a6274022d6a3bd4c8de08abcea51bc4164
jmlynch <jmlynch@gmail.com>
2016-01-22 10:17:21 -0500

    Update Auto.py

--- Auto.py
+++ Auto.py
@@ -9,11 +9,11 @@ class Auto(BotPlugin):
         super(Auto, self).activate()
 
 	try:
-		Popen(['/usr/bin/automater/Automater.py -b'], stdout=PIPE, stderr=STDOUT).communicate()[0]
+		Popen(['/usr/bin/automater/Automater.py -b -r'], stdout=PIPE, stderr=STDOUT).communicate()[0]
 	except Exception as e:
 		self.warn_admins("This plugin uses automater and it wasn't found on your system")
 
    @botcmd
    def Auto(self, mess, args):
-      return Popen(['/usr/bin/automater/Automater.py -b'] + args.split(), stdout=PIPE, stderr=STDOUT).communicate()[0].decode('utf-8')
+      return Popen(['/usr/bin/automater/Automater.py -b -r'] + args.split(), stdout=PIPE, stderr=STDOUT).communicate()[0].decode('utf-8')
 	
