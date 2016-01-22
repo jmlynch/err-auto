488f08f6c89a843cec298df6a166c021b828b97d
jmlynch <jmlynch@gmail.com>
2016-01-22 09:40:02 -0500

    Update and rename whois.py to Auto.py

---
+++ Auto.py
@@ -0,0 +1,19 @@
+from errbot import BotPlugin, botcmd
+from subprocess import Popen, PIPE, STDOUT
+
+
+class Auto(BotPlugin):
+   max_err_version = '9.9.9' # Optional, but recommended
+
+   def activate(self):
+        super(Auto, self).activate()
+
+	try:
+		Popen(['/usr/bin/automater/Automater -b'], stdout=PIPE, stderr=STDOUT).communicate()[0]
+	except Exception as e:
+		self.warn_admins("This plugin uses whois and it wasn't found on your system")
+
+   @botcmd
+   def Auto(self, mess, args):
+      return Popen(['/usr/bin/automater/Automater -b'] + args.split(), stdout=PIPE, stderr=STDOUT).communicate()[0].decode('utf-8')
+	
